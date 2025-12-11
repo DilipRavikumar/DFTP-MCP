e56da285149ef78d97a0be3b1d8a98879fd94e61
infi-nithin <infi.nithin28@gmail.com>
2025-12-11 20:42:37 +0530

    intialized the branch

--- Authorization_Agent/schema.py
+++
@@ -1,8 +0,0 @@
-from typing import List, TypedDict, Optional
-
-class State(TypedDict):
-    prompt: str
-    token: str
-    result: Optional[str]
-    scope: Optional[str]
-    roles: Optional[List[str]]
