d6a56381d87767cfdb02592b481122635b948156
Dilip R <dilip@example.com>
2026-01-12 15:45:12 +0530

    added role in sidebar

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -5,46 +5,47 @@ import { Observable, tap } from 'rxjs';
 import { jwtDecode } from 'jwt-decode';
 
 interface JwtPayload {
-    scope?: string;
+  scope?: string;
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-
-@Injectable({ providedIn: 'root' })
 export class AuthService {
   private apiUrl = 'http://localhost:8081/api';
   isAuthenticated = signal(false);
+  currentUserRoles = signal<string[]>([]);
   currentUserScope = signal<string>('General');
-  constructor(private http: HttpClient, private router: Router) {}
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   login() {
     window.location.href = `${this.apiUrl}/auth/login`;
   }
 
-logout() {
-     this.isAuthenticated.set(false);
-  window.location.href = `${this.apiUrl}/auth/logout`;
-}
+  logout() {
+    this.isAuthenticated.set(false);
+    window.location.href = `${this.apiUrl}/auth/logout`;
+  }
 
- me() {
+  me() {
     return this.http.get<any>(`${this.apiUrl}/auth/me`, {
       withCredentials: true
     });
   }
 
   fetchUser() {
-  this.me().subscribe({
-    next: (res) => {
-      this.isAuthenticated.set(res.authenticated);
-      this.currentUserScope.set(res.scope); 
-    },
-    error: () => {
-      this.isAuthenticated.set(false);
-      this.currentUserScope.set('General'); 
-    }
-  });
-}
-
+    this.me().subscribe({
+      next: (res) => {
+        this.isAuthenticated.set(res.authenticated);
+        this.currentUserScope.set(res.scope);
+        this.currentUserRoles.set(res.roles || []);
+      },
+      error: () => {
+        this.isAuthenticated.set(false);
+        this.currentUserScope.set('General');
+        this.currentUserRoles.set([]);
+      }
+    });
+  }
 }
