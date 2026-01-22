00f396da74778ee5fcfac37e2a096140d9dcc27d
NIKITHAPALANISAMY412 <nikithapalanisamy412@gmail.com>
2026-01-22 18:33:40 +0530

    Minor Change

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,21 +4,6 @@ import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { LoginCallbackComponent } from './features/logincallback/logincallback.component';
 import { ChatComponent } from './features/chat/chat.component';
 
-// export const routes: Routes = [
-//     {
-//         path: '', component: DashboardComponent
-//     },
-//     {
-//         path: 'login',
-//         loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent)
-//     },
-//     {
-//         path: 'chat',
-//         loadComponent: () => import('./features/chat/chat.component').then(m => m.ChatComponent),
-//         canActivate: [authGuard]
-//     }
-// ];
-
 export const routes = [
   { path: '', component: DashboardComponent },
   { path: 'login-callback', component: LoginCallbackComponent },
